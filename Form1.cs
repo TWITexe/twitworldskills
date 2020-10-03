6f4939a3fa5f9db63ee386fc38e83e74d6705b09
TWITexe <kawaii_senpaii@mail.ru>
2020-10-03 16:28:35 +0700

    source code

---
+++ Form1.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace twitdatabase
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string name = textBox1.Text;
+            string sex = textBox2.Text;
+            string numberphone = textBox3.Text;
+            string address = textBox4.Text;
+            dataGridView1.Rows.Add(name, sex, numberphone, address);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            dataGridView1.Rows.Clear();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int ind = dataGridView1.SelectedCells[0].RowIndex;
+                dataGridView1.Rows.RemoveAt(ind);
+            } catch(Exception ex)
+                    {
+                MessageBox.Show(ex.Message);
+            }
+
+        }
+
+        private void открытьToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Stream mystr = null;
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                if (( mystr = openFileDialog1.OpenFile()) != null)
+                    {
+                    StreamReader myread = new StreamReader(mystr);
+                    string[] str;
+                    int num = 0;
+                    try
+                    {
+                        string[] strl = myread.ReadToEnd().Split('\n');
+                        num = strl.Count();
+                        dataGridView1.RowCount = num;
+                        for (int i = 0; i < num; i ++ )
+                        {
+                            str = strl[i].Split('^');
+                            for (int l = 0; l < dataGridView1.ColumnCount; l++)
+                            {
+                                try
+                                {
+                                    dataGridView1.Rows[i].Cells[l].Value = str[l];
+                                }
+                                catch { }
+                            }
+                                
+                        }
+                    }
+                    catch (Exception ex )
+                    {
+                        MessageBox.Show(ex.Message);
+                    }
+                    finally { myread.Close();}
+                }
+            }
+        }
+
+        private void сохранитьКакToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Stream myStream;
+            if (saveFileDialog1.ShowDialog() == DialogResult.OK )
+            {
+                if ((myStream = saveFileDialog1.OpenFile()) != null)
+                {
+                    StreamWriter myWritet = new StreamWriter(myStream);
+                    try
+                    {
+                        for (int i = 0; i < dataGridView1.RowCount - 1; i++)
+                        {
+                            for (int j = 0; j < dataGridView1.ColumnCount; j++)
+                            {
+                                myWritet.Write(dataGridView1.Rows[i].Cells[j].Value.ToString() + "^");
+                            }
+                            myWritet.WriteLine();
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show(ex.Message);
+                    }
+                    finally
+                    {
+                        myWritet.Close();
+                    }
+                    myStream.Close();
+                }
+
+            }
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = null;
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            textBox2.Text = null;
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            textBox3.Text = null;
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            textBox4.Text = null;
+        }
+    }
+}
---
+++ math.cs
@@ -0,0 +1,91 @@
+﻿using System;
+
+namespace twit
+{
+    public class math
+    {
+        static public void summ(double x, double y)
+        {
+            double summ = x + y;
+            Console.WriteLine("Сумма = " + summ);
+            Console.ReadLine();
+        }
+        static public void min(double x, double y)
+        {
+            double min = x - y;
+            Console.WriteLine("Разность = " + min);
+            Console.ReadLine();
+        }
+        static public void mult(double x, double y)
+        {
+            double mult = x * y;
+            Console.WriteLine("Произведение = " + mult);
+            Console.ReadLine();
+        }
+        static public void div(double x, double y)
+        {
+            double div = x / y;
+            Console.WriteLine("Частное = " + div);
+            Console.ReadLine();
+        }
+        static public void sbox(double x)
+        {
+            double sbox = Math.Pow(x, 2);           
+            Console.WriteLine("S квадрата по стороне = " + sbox);
+            Console.ReadLine();
+        }
+        static public void sboxdiag(double x)
+        {
+            double sboxdiag = Math.Pow((x/2), 2);
+            Console.WriteLine("S квадрата по его диагонали = " + sboxdiag);
+            Console.ReadLine();
+        }
+
+        static public void scircleR(double r)
+        {
+            double scircle = Math.PI * Math.Pow(r, 2);
+            Console.WriteLine("S круга через радиус = " + scircle);
+            Console.ReadLine();
+        }
+        static public void scircleD(double d)
+        {
+            double scircle = Math.PI * (Math.Pow(d, 2) / 4 );
+            Console.WriteLine("S круга через диаметр = " + scircle);
+            Console.ReadLine();
+        }
+        static public void sparall(double a, double h)
+        {
+            double sparall = a * h;
+            Console.WriteLine("Длина = " + a);
+            Console.WriteLine("Высота = " + h);
+            //Площадь параллелограмма равна произведению длины его стороны и длины опущенной на эту сторону высоты.
+            Console.WriteLine("S параллелограмма по длине стороны и высоте = " + sparall);
+            Console.ReadLine();
+        }
+
+        static public void sparallsin(double a, double b, double sin)
+        {
+            double sinus = Math.PI * sin / 180; // перевод радиан в градусы.
+            double sparallsin = a * b * Math.Sin(sinus);  // Нахождение площади.
+            Console.WriteLine("Сторона 1 = " + a);
+            Console.WriteLine("Сторона 2 = " + b);
+            Console.WriteLine("Sin = " + Math.Sin(sinus));
+            //Площадь параллелограмма равна произведению длин его сторон умноженному на синус угла между ними.
+            Console.WriteLine("S параллелограмма по двум сторонам и углу между ними = " + sparallsin);
+            Console.ReadLine();
+        }
+        static public void sparalldiag(double a, double b, double sin)
+        {
+            
+            double sinus = Math.PI * sin / 180;
+            double sparalldiag = 1/2 * (a * b * Math.Sin(sinus));
+            Console.WriteLine("Диагональ 1 = " + a);
+            Console.WriteLine("Диагональ 2 = " + b);
+            Console.WriteLine("Sin(a) = " + Math.Sin(sinus));
+            //Площадь параллелограмма равна половине произведения длин его диагоналей умноженному на синус угла между ними.
+            Console.WriteLine("S параллелограмма по двум диагоналям и углу между ними = " + sparalldiag);
+            Console.ReadLine();
+        }
+
+    }
+}
